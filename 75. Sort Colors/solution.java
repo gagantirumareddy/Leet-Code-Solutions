2023aa5c5603458a7eee45f5cdb6a34d8ee540ef
gagantirumareddy <gagantirumareddy@gmail.com>
2026-02-25 22:34:51 +0530

    Time : 0 ms (100%)  | Memory : 43.6 MB (60.97%)

--- 75. Sort Colors/solution.java
+++ 75. Sort Colors/solution.java
@@ -1,7 +1,6 @@
 class Solution {
     public void sortColors(int[] nums) {
         int low = 0, mid = 0, high = nums.length - 1;
-
         while (mid <= high) {
             if (nums[mid] == 0) {
                 int temp = nums[mid];
